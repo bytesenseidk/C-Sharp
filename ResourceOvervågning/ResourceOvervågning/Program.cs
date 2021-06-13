e1bc6ba5af1145f5c50c69509778ef714819c48d
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-06-13 21:33:32 +0200

    update

--- ResourceOvervågning/ResourceOvervågning/Program.cs
+++ ResourceOvervågning/ResourceOvervågning/Program.cs
@@ -5,8 +5,8 @@ using ConsoleTables;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 
-// Nødvendig package der installeres under Tools > NuGet Package Manager > Package Manager Console
-// Under konsolen, skriv: Install-Package ConsoleTables -Version 2.4.2
+/* Nødvendig package der installeres under "Tools > NuGet Package Manager > Package Manager Console"
+   Under konsolen, skriv: "Install-Package ConsoleTables -Version 2.4.2" */
 
 namespace ResourceOvervågning
 {
@@ -14,39 +14,34 @@ namespace ResourceOvervågning
     {
         static void Main(string[] args)
         {
-            // Opdateringstid: 1 min = 60.000 ms.
-            int sleeptime = 5 * 60000;
-
-            // Uendeligt loop
-            while (true)
+            while (true) // Uendeligt loop
             {
-                // Print Resource Tabel.
-                ResourceData();
-                // Vent 5 Minutter.
-                System.Threading.Thread.Sleep(sleeptime);
+                ResourceData(); // Print Resource Tabel.
             }
         }
 
         static void ResourceData()
         {
-            // API Instans.
-            DVIMonitor.monitorSoapClient ds = new DVIMonitor.monitorSoapClient();
+            DVIMonitor.monitorSoapClient ds = new DVIMonitor.monitorSoapClient(); // API Instans.
             
+            int sleeptime = 5 * 60000; // Opdateringstid: 1 min = 60.000 ms.
+
             /* Lager Informationer */
             string mest_solgt = "";
             string over_max   = "";
-            // Liste indeholdende stringe (Arrays kan ikke tillægges nye værdier)
-            List<string> under_min = new List<string>();
-            // Tillæg div. værdier til variablerne.
-            foreach(string element in ds.StockItemsMostSold()) { mest_solgt += $"{element.Trim()}"; }
+            
+            List<string> under_min = new List<string>(); // Liste indeholdende stringe (Arrays kan ikke tillægges nye værdier)
+            
+            /* Tillæg div. værdier til variablerne. */
+            foreach (string element in ds.StockItemsMostSold()) { mest_solgt += $"{element.Trim()}"; }
             foreach(string element in ds.StockItemsOverMax()) { over_max += $"{element.Trim()}"; }
             foreach (string element in ds.StockItemsUnderMin()) { under_min.Add(element); }
             string[] u_min = under_min.ToArray();
-            // Lager Tabel.
-            var lager = new ConsoleTable("Mest Solgte Varer: ", mest_solgt);
-            lager.AddRow("Varer Under Minimum: ", under_min[0])
-                 .AddRow("", under_min[1])
-                 .AddRow("Varer Over Maximum: ", over_max);
+            
+            var lager = new ConsoleTable("Mest Solgte Varer: ", mest_solgt); // Lager Tabel.
+            lager.AddRow("Varer Over Maximum: ", over_max)
+                 .AddRow("Varer Under Minimum: ", under_min[0])
+                 .AddRow("", under_min[1]);
             lager.Write(Format.Alternative);
 
             /* Klima Informationer */
@@ -54,11 +49,13 @@ namespace ResourceOvervågning
             string lager_fugtighed = $"Lager: {ds.StockHumidity()}%";
             string ude_temp        = $"Ude: {ds.OutdoorTemp()}°C";
             string ude_fugtighed   = $"Ude: {ds.OutdoorHumidity()}%";
-            // Klima Tabel
-            var klima = new ConsoleTable("[ TEMPERATUR ]", "[ FUGTIGHED ]");
+            
+            var klima = new ConsoleTable("[ TEMPERATUR ]", "[ FUGTIGHED ]"); // Klima Tabel
             klima.AddRow(lager_temp, lager_fugtighed)
                  .AddRow(ude_temp, ude_fugtighed);
             klima.Write(Format.Alternative);
+
+            System.Threading.Thread.Sleep(sleeptime); // Vent 5 Minutter.
 
             return;
         }
