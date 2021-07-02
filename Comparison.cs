97553b270d0987deb14f69eeb59fea82990df01a
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-07-02 08:09:30 +0000

    Add files via upload

--- Comparison.cs
+++ Comparison.cs
@@ -2,8 +2,9 @@ using System;
 
 namespace CSharp 
 {
-    class Comparison 
+    class Comparison
     {
+        /* Compare two given strings*/ 
         static void Main(string[] args) 
         {
             string string_1 =  "Hello!";
@@ -13,16 +14,18 @@ namespace CSharp
         }
         static bool Compare(string string_1, string string_2) 
         {
-            int counter = 0;
-            bool validation = false;
-            if (string_1.Length != string_2.Length) 
+            int counter = 0; // Index for second string
+            bool validation = false; // Default string equality set to false
+            if (string_1.Length != string_2.Length)
             {
+                // If the given strings ain't of the same length, they cant be equal
                 validation = false;
             }
             else 
             {
-                foreach (char value in string_1) 
+                foreach (char value in string_1)
                 {
+                    // Loops through each character and test if they match each other
                     if (value == string_2[counter]) 
                     {
                         validation = true;
@@ -30,12 +33,14 @@ namespace CSharp
                     else 
                     {
                         validation = false;
+                        // If characters dont match, we end the loop and returns false
                         return validation;
                     }
-                counter++;
+                counter++; // If the character of each string match, we increase our counter and continue the loop
                 }
             }
             return validation;
         }
     }
 }
+
