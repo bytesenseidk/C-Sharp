f263c0aade358d714e448015e3aea93e235f23ec
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-07-02 10:08:33 +0200

    major

--- Comparison.cs
+++ Comparison.cs
@@ -1,32 +1,42 @@
 using System;
 
-namespace CSharp {
-    class Comparison {
-        static void Main(string[] args) {
-            string string_1 = "Hello!";
-            string string_2 = "Hello!";
-            string string_3 = "coding";
-            bool result_1 = Compare(string_1, string_2);
-            bool result_2 = Compare(string_1, string_3);
-            System.Console.WriteLine($"Is '{string_1}' equal to '{string_2}': {result_1}");
-            System.Console.WriteLine($"Is '{string_1}' equal to '{string_3}': {result_2}");
+namespace CSharp 
+{
+    class Comparison
+    {
+        /* Compare two given strings*/ 
+        static void Main(string[] args) 
+        {
+            string string_1 =  "Hello!";
+            string string_2 =  "Hello!";
+            bool result = Compare(string_1, string_2);
+            System.Console.WriteLine(result);
         }
-        static bool Compare(string string_1, string string_2) {
-            int counter = 0;
-            bool validation = false;
-            if (string_1.Length != string_2.Length) {
+        static bool Compare(string string_1, string string_2) 
+        {
+            int counter = 0; // Index for second string
+            bool validation = false; // Default string equality set to false
+            if (string_1.Length != string_2.Length)
+            {
+                // If the given strings ain't of the same length, they cant be equal
                 validation = false;
             }
-            else {
-                foreach (char value in string_1) {
-                    if (value == string_2[counter]) {
+            else 
+            {
+                foreach (char value in string_1)
+                {
+                    // Loops through each character and test if they match each other
+                    if (value == string_2[counter]) 
+                    {
                         validation = true;
                     }
-                    else {
+                    else 
+                    {
                         validation = false;
+                        // If characters dont match, we end the loop and returns false
                         return validation;
                     }
-                counter++;
+                counter++; // If the character of each string match, we increase our counter and continue the loop
                 }
             }
             return validation;
