b4a43742a4f91fe25e541f45fdce7489583952d1
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-07-03 11:35:54 +0200

    Major

---
+++ UserSystem/User System/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace User_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
