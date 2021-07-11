b366d4b4de97d6358b9ed95324719b5e01e1f00b
LarsRosenkilde <lars.rosenkilde95@gmail.com>
2021-07-11 10:58:54 +0200

    Initial Test Area

---
+++ TestArea/TestArea/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
