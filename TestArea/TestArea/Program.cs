3610175970a68acf5d19c8c3b01d6ab3cb48cfc1
LarsRosenkilde <lars.rosenkilde95@gmail.com>
2021-07-11 12:06:01 +0200

    Update

--- TestArea/TestArea/Program.cs
+++ TestArea/TestArea/Program.cs
@@ -1,20 +1,100 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace TestArea
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] questionData = Addition();
-            AskQuestion(questionData[0], questionData[1], questionData[2]);
+            string operatorChoice = Operation();
+            if (operatorChoice == "Exit") { System.Environment.Exit(0); }
+            else if (operatorChoice == "Addition") { Addition(); }
+            else if (operatorChoice == "Subtraction") { Subtraction(); }
         }
 
-        static void AskQuestion(int num1, int num2, int result)
+        static string Operation()
         {
-            string question = $"{num1} + {num2} = ";
+            string str_choice;
+            Dictionary<int, string> operators = new Dictionary<int, string>()
+            {
+                {0, "Exit"},
+                {1, "Addition"},
+                {2, "Subtraction"},
+                {3, "Multiplication"},
+                {4, "Division"}
+            };
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("\n[ Operation Selection ]\n");
+                foreach (KeyValuePair<int, string> kvp in operators)
+                {
+                    Console.WriteLine($"[{kvp.Key}] {kvp.Value}");
+                }
+                try
+                {
+                    Console.Write("\nEnter choice: ");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    str_choice = operators[choice];
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("\n\nInvalid data, need numeric input within the menu scope..");
+                    Console.ReadKey();
+                    continue;
+                }
+            }
+            return str_choice;
+        }
 
-            Console.Clear();
+        static void Addition()
+        {
+            int num1; int num2;
+            int result;
+            int score = 0;
+            Random number = new Random();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"\n[ Addition ]\nScore: {score}");
+
+                num1 = number.Next(10, 1000);
+                num2 = number.Next(10, 1000);
+                result = num1 + num2;
+
+                string question = $"{num1} + {num2} = ";
+
+                Console.Write(question);
+                int answer = Convert.ToInt32(Console.ReadLine());
+
+                if (answer == result)
+                {
+                    Console.WriteLine("\nCorrect!");
+                    score++;
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("\nIncorrect!");
+                    break;
+                }
+            }
+        }
+
+        static void Subtraction()
+        {
+            int num1; int num2;
+            int result;
+            Random number = new Random();
+
+            num1 = number.Next(10, 1000);
+            num2 = number.Next(10, 1000);
+            result = num1 - num2;
+
+            string question = $"\n{num1} - {num2} = ";
+
             Console.Write(question);
             int answer = Convert.ToInt32(Console.ReadLine());
 
@@ -26,22 +106,6 @@ namespace TestArea
             {
                 Console.WriteLine("Incorrect!");
             }
-        }
-
-        static int[] Addition()
-        {
-            int num1; int num2; 
-            int result;
-            Random number = new Random();
-
-            num1 = number.Next(10, 1000);
-            num2 = number.Next(10, 1000);
-            
-            result = num1 + num2;
-
-            int[] data = { num1, num2, result };
-            
-            return data;
         }
     }
 }
