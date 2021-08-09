fcaba833c1be7bb48200db73bfcddc59d3f1d8d5
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-08-09 14:11:42 +0200

    BMI Calculator

---
+++ BodyMassIndex/BodyMassIndex/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace BodyMassIndex
+{
+    // Super Class
+    class BaseClass
+    {
+        private double weight;
+        private double height;
+
+        public double Weight
+        {
+            get { return weight; }
+            set
+            {
+                weight = value < 1 ? 100 : value;
+            }
+        }
+        public double Height
+        {
+            get { return height; }
+            set 
+            { 
+                height = value < 1 ? 100 : value; 
+            }
+        }
+
+        public virtual string Values()
+        {
+            return $"Weight: {Weight}\nHeight: {Height}";
+        }
+    }
+    // Base Class
+    class DerivedClass : BaseClass
+    {
+        public DerivedClass(double weight, double height)
+        {
+            Weight = weight;
+            Height = height;
+        }
+        public double BMI()
+        {
+            return Math.Round(Weight / Height / Height * 10_000, 2);
+        }
+        // Polymorphism
+        public override string Values()
+        {
+            return base.Values() + $"\nBMI: {BMI()}";
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass Bmi = new(100, 175);
+            Console.WriteLine(Bmi.Values());   
+        }
+    }
+}
