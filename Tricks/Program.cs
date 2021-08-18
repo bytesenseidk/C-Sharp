a873ec9bd072508206ed110c5d7378fca9edab94
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-08-18 09:41:37 +0200

    push

---
+++ Tricks/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+/* Content:
+- String formatting
+
+*/
+
+namespace Tricks
+{
+    class StringFormatting 
+    {
+        public string someStr = "C#";
+        public int someInt = 7;
+        public decimal someDec = 2.7M;
+        public string someDate = DateTime.Now.ToString("dd':'MM':'yyyy");
+
+        public void StandardFormat() 
+        {
+            WriteLine(
+                format: "This is coded in:\t{0}\nHere is a currency:\t{1:C}\nToday's date:\t\t{2}",
+                arg0: someStr,
+                arg1: someInt * someDec,
+                arg2: someDate);
+        }
+        public void InterpolatedFormat()
+        {
+            WriteLine($"This is coded in:\t{someStr}\nHere is a currency:\t{someInt * someDec:C}\nToday's date:\t\t{someDate}");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringFormatting formatExample = new();
+            formatExample.StandardFormat();
+            System.Console.WriteLine();
+            formatExample.InterpolatedFormat();
+        }
+    }
+}
