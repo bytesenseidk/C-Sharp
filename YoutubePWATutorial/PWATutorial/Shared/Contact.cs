68b315817f41d2130dcc338107da9012159c0817
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-08-18 12:27:48 +0200

    PWA analysis

---
+++ YoutubePWATutorial/PWATutorial/Shared/Contact.cs
@@ -0,0 +1,24 @@
+﻿namespace PWATutorial.Shared
+{
+    public class Contact
+    {
+        public int ContactID {  get; set; }
+        public string FirstName {  get; set; }
+        public string LastName {  get; set; }
+
+        // public Contact() 
+        // {
+
+        // }
+        public Contact(int ContactID, string FirstName, string LastName)
+        {
+            this.ContactID = ContactID;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+        public string FullName()
+        {
+            return $"{this.FirstName} {this.LastName}";
+        }
+    }
+}
