4856b5a1f5c08f7bb5eaa15ad34c5630c8b30208
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-08-03 12:40:39 +0200

    Update FPSMovement.cs

--- GameScripts/FPSMovement.cs
+++ GameScripts/FPSMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FPSMovement : MonoBehaviour
 {
     public float speed = 6.0f;
-    
+    public float gravity = -9.8f;
     private CharacterController _controller;
 
     void Start() 
@@ -19,6 +19,7 @@ public class FPSMovement : MonoBehaviour
         float deltaZ = Input.GetAxis("Vertical") * speed;
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
         movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
         movement *= Time.deltaTime;
         movement = transform.TransformDirection(movement);
         _controller.Move(movement);
