94dfefc6d2e3d75bd9511fc213cd42374cff31e4
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-08-03 11:39:43 +0200

    Create MouseLook.cs

---
+++ GameScripts/MouseLook.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes 
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+    private float _rotationX = 0;
+
+    void Update()
+    {
+        if (axes == RotationAxes.MouseX) 
+        {
+            // Horizontal rotation
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+        }
+        else if (axes == RotationAxes.MouseY) 
+        {
+            // Vertical rotation
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+        else 
+        {
+            // Both horizontal and vertical rotation
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+            float rotationY = transform.localEulerAngles.y + delta;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+    }
+}
