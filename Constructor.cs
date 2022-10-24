f851c2ae554e5104613692f15d103edb8ef4fcc5
LarsRosenkilde <lars.rosenkilde95@gmail.com>
2022-10-24 11:21:51 +0000

    New small snipits

---
+++ Constructor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Mixed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicArithmetic multi = new BasicArithmetic(7, 9);
+            System.Console.WriteLine(multi.addition());
+        }
+    }
+
+    public class BasicArithmetic
+    {
+        private double a;
+        private double b;
+
+        public BasicArithmetic(double num_0, double num_1)
+        {
+            a = num_0;
+            b = num_1;
+        }
+
+        public double addition()
+        {
+            return a + b;
+        }
+    }
+}
